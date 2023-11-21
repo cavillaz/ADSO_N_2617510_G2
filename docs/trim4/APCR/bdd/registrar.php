beee4858e2916149086d32a1523d029df2366692
soliyen <124852329+soliyen@users.noreply.github.com>
2023-11-20 21:02:37 -0500

    bdd

---
+++ docs/trim4/APCR/bdd/registrar.php
@@ -0,0 +1,32 @@
+<?php
+require("conexion.php")
+    
+     if ($conexion->connect_error) {
+         die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+     }
+ 
+     // Verifica si el formulario fue enviado
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+         // Recupera los datos del formulario
+         $nombres = $_POST["nombres"];
+         $identificacion = $_POST["identificacion"];
+         $apartamento = $_POST["apartamento"];
+         $torre = $_POST["torre"];
+         $celular = $_POST["celular"];
+         $correo = $_POST["correo"];
+         $clave = $_POST["clave"];
+         
+         // Prepara la consulta SQL para la inserción
+         $sql = "INSERT INTO residente (nombres, identificacion, apartamento, torre, celular, correo, clave) VALUES ('$nombres', '$identificacion', '$apartamento', '$torre', '$celular', '$correo', '$clave')";
+         
+ 
+         // Ejecuta la consulta
+         if ($conexion->query($sql) === TRUE) {
+             echo "Registro exitoso";
+         } else {
+             echo "Error en el registro: " . $conexion->error;
+         }
+     }
+ 
+     // Cierra la conexión a la base de datos
+     $conexion->close();
